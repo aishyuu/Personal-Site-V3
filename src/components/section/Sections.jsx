035c5b945f156a67372fbbd18f4abaabbde70baa
aishyuu <ivanmatias1331@gmail.com>
2023-01-14 18:31:52 -0800

    Fixed readme and added linkedin link

--- src/components/section/Sections.jsx
+++ src/components/section/Sections.jsx
@@ -1,12 +1,17 @@
 import {sectionsData} from '../../data/sections.js'
 import "./Sections.css"
+import React from 'react'
 
 export default function Sections() {
+    const [modalOn, setModalOn] = React.useState(false)
+
     return(
         <div className='sections'>
+            {/* Go through each entry of sections */}
             {sectionsData.map(section => {
                 if(section.is_link){
                     return(
+                        //What shows if it's a link
                         <a className="section-link" href={section.link} target="_blank" rel='noreferrer'>
                             <div className='section-indiv'>
                                 <img className="section-image" src={section.image} alt="" srcset="" />
@@ -17,9 +22,11 @@ export default function Sections() {
                     )
                 } else {
                     return(
+                        //If it's not a link, we will set up a modal.
                         <div className='section-indiv'>
                             <img className="section-image" src={section.image} alt="" srcset="" />
                             <h3>{section.name}</h3>
+                            <h3>{modalOn}</h3>
                         </div>
                     )
                 }
--- src/data/sections.js
+++ src/data/sections.js
@@ -12,13 +12,20 @@ export const sectionsData = [
     {
         name: "My Blog",
         image: "images/program.png",
-        is_link: false,
+        is_link: true,
+        link: "https://ivanmatias.netlify.app/blog"
     },
     {
         name: "My Github",
         image: "images/github.png",
         is_link: true,
         link: "https://github.com/aishyuu"
+    },
+    {
+        name: "Linkedin",
+        image: "images/linkedin.png",
+        is_link: true,
+        link: "https://www.linkedin.com/in/ivan-matias-1331im/"
     },
     {
         name: "Source Code",
